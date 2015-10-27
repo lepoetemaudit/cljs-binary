07d6c33c25cce37634e41d576ca09c410fd64733
lepoetemaudit <mail@davidjeffrey.co.uk>
2015-10-27 00:19:41 +0000

    Committing actual files

---
+++ src/binary/core.cljs
@@ -0,0 +1,98 @@
+(ns ^:figwheel-always binary.core
+    (:require [clojure.string :as string]))
+
+(enable-console-print!)
+
+(println "Edits to this text should show up in your developer console.")
+
+;; define your app data so that it doesn't get over-written on reload
+
+(defonce app-state (atom {:text "Hello world!"}))
+
+(defn char-array-to-ab [string]
+    (let [strlen (.-length string)
+          buf (new js/ArrayBuffer strlen)
+          arr (new js/Uint8Array buf)]
+
+        (loop [i 0] (when (< i strlen)
+                        (aset arr i  (.charCodeAt string i))
+                        (recur (inc i))))
+
+
+        buf))
+
+(def js-types {:uint8 {:size 8
+                       :js-get "getUint8"}
+
+               :bit {:size 1
+                     :js-get "getUint8"
+                     :process (fn [value pos]
+                                (println "pos is" (mod pos 8))
+                                (println "value is" value)
+                                (bit-test value (mod pos 8)))}})
+
+(def binary-types {
+                   :charstring {:sizable true
+                                :js-type :uint8
+                                :formatter #(string/join (map js/String.fromCharCode %))}
+                   :uint8       {:js-type :uint8}
+                   :bit         {:js-type :bit}
+                   :ubitnum     {:js-type :bit
+                                 :sizable true}})
+
+(defn parse-type-key [key]
+    (let [parts (.split (name key) "-")
+          typ ((keyword (first parts)) binary-types)
+          length (if (:sizable typ) (js/parseInt (second parts)) 1)
+          js-type ((:js-type typ) js-types)]
+
+        (assoc typ :length length :js-type js-type :le (= :le (:last parts)))))
+
+
+(defn no-process [val _] val)
+
+(defn read-data [dv {:keys [length formatter le js-type], :or {formatter no-process}} index]
+  (println "type size:" (< (:size js-type) 8))
+  (let [get-func (.bind (aget dv (:js-get js-type)) dv)
+        adjusted-index (/ index 8)
+        {:keys [size process] :or {process identity}} js-type]
+
+    (println "adjusted-index is" adjusted-index)
+    [(* length size)
+     (formatter
+       (if (> length 1)
+         (loop [pos 0 data []]
+           (if (= pos length)
+             data
+             (recur (inc pos) (conj data (process
+                                           (get-func (/ (+ index pos) 8) le)
+                                           (+ index pos))))))
+         (process (get-func (/ index 8) le) index)))]))
+
+(defn read-spec [spec]
+    (let [buf (char-array-to-ab "halohhwareyou")
+          dv (new js/DataView buf)
+          partitioned (partition 2 spec)]
+
+        (loop [pos 0 items partitioned output {}]
+            (let [[item definition] (first items)
+                  parsed-key (parse-type-key definition)
+                  [size value] (read-data dv parsed-key pos)
+                  out (assoc output item value)]
+
+              (if (> (count items) 1)
+                  (recur (+ pos size) (drop 1 items) out)
+                  out)))))
+
+
+
+(defn on-js-reload []
+    (println (read-spec [:name :charstring-4
+                         :two :uint8
+                         :smallnum :ubitnum-13
+                         ]))
+
+    ;; optionally touch your app-state to force rerendering depending on
+    ;; your application
+    ;; (swap! app-state update-in [:__figwheel_counter] inc)
+    )
